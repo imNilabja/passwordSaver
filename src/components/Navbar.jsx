d556c3e9d7f7873e2c17c6d3f680466552c110b9
iamNilabja <goswamineel23.com>
2024-07-21 00:40:37 +0530

    margin adjusted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <>
 
-      <nav className='bg-black flex h-14 items-center justify-between px-10'>
+      <nav className='bg-black flex h-14 items-center justify-between px-5'>
         <div className="logo font-bold text-xl">
           <span className="text-green-500">&lt;</span>
           <span className="text-white">Pass</span>
@@ -12,7 +12,7 @@ export const Navbar = () => {
           <span className="text-green-500">/&gt;</span>
         </div>
         <ul>
-          <li className='flex w-[200px] justify-between text-white'>
+          <li className='flex w-[200px] justify-between text-white mx-3'>
             <a className='hover:font-bold' href="#">Home</a>
             <a className='hover:font-bold' href="#">About</a>
             <a className='hover:font-bold' href="#">contact</a>
